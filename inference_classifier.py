cdc63f6f14431e5878c5dcf23181b1d7d077af0f
Swaghaug <79215866+Swaghaug@users.noreply.github.com>
2023-07-06 17:48:35 +0200

    Add files via upload
    Classifier that also counts score if the dict labels are named 'Increase point' and 'Deduct point'.

---
+++ inference_classifier.py
@@ -0,0 +1,92 @@
+import pickle
+import time
+import cv2
+import mediapipe as mp
+import numpy as np
+
+model_dict = pickle.load(open('./model.p', 'rb'))
+model = model_dict['model']
+
+cap = cv2.VideoCapture(0)
+
+#Scorebox
+scoreDelay = 3 #time between possible score appends
+score = 0
+last_execution_time = time.time()
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.9)
+
+labels_dict = {0: 'Increase point', 1: 'Deduct point'}
+while True:
+
+    data_aux = []
+    x_ = []
+    y_ = []
+
+    ret, frame = cap.read()
+
+    H, W, _ = frame.shape
+
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    results = hands.process(frame_rgb)
+    
+    if results.multi_hand_landmarks:
+        #print(results.multi_hand_landmarks)
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                frame,  # image to draw
+                hand_landmarks,  # model output
+                mp_hands.HAND_CONNECTIONS,  # hand connections
+                mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style())
+
+        for hand_landmarks in results.multi_hand_landmarks:
+            for i in range(len(hand_landmarks.landmark)):
+                x = hand_landmarks.landmark[i].x
+                y = hand_landmarks.landmark[i].y
+
+                x_.append(x)
+                y_.append(y)
+
+            for i in range(len(hand_landmarks.landmark)):
+                x = hand_landmarks.landmark[i].x
+                y = hand_landmarks.landmark[i].y
+                data_aux.append(x - min(x_))
+                data_aux.append(y - min(y_))
+
+        x1 = int(min(x_) * W) - 10
+        y1 = int(min(y_) * H) - 10
+
+        x2 = int(max(x_) * W) - 10
+        y2 = int(max(y_) * H) - 10
+
+        prediction = model.predict([np.asarray(data_aux)])
+
+        predicted_character = labels_dict[int(prediction[0])]
+
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
+        if (predicted_character=='Increase point') and (time.time() - last_execution_time>scoreDelay):
+            score +=1
+            print(f'Score: {score}')
+            last_execution_time = time.time()
+            
+
+        if (predicted_character=='Deduct point')and (time.time() - last_execution_time>scoreDelay):
+            score -=1
+            print(f'Score: {score}')
+            last_execution_time = time.time()
+        cv2.putText(frame, predicted_character, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3,
+                    cv2.LINE_AA)
+
+
+    cv2.imshow('Scorebox', frame)
+    cv2.waitKey(1)
+
+
+cap.release()
+cv2.destroyAllWindows()
